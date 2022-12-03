61680b2c92f02dc740b956ab1f8579031d62de40
Franco Erben <francoerben@hotmail.com.ar>
2022-12-03 14:07:04 -0300

    a corregir

--- ERBShop-Franco Erben/src/components/CartContext.jsx
+++ ERBShop-Franco Erben/src/components/CartContext.jsx
@@ -8,22 +8,25 @@ export const CartContext = createContext()
 const CartContextProvider = (props )=> {
  
     const [cartList,setCartList] = useState([])
-const addToCart=(props , qty)=>{
-    let found = cartList.find(product=>product.id===props.id)
-    if(!found){
-    setCartList([...cartList,
-        {id: props.id,
-        title: props.title,
-        stock: props.stock,
-        price: props.price,
-          qty:qty,
-       img: props.thumbnail  }])
-
-        }else{
-            setCartList([...cartList, {...found, qty: qty+qty}])
-
-            
-        }}
+    const addToCart=(props , qty)=>{
+        let found = cartList.find(product=>product.id===props.id)
+        if(found){
+            const updatedCart = cartList.map((prod)=>{
+                if(prod.id === props.id){
+                    return {...prod, qty:prod.qty + qty}
+                }
+            })
+            setCartList(updatedCart)
+    
+            }else{
+                setCartList([...cartList,
+                    {id: props.id,
+                    title: props.title,
+                    stock: props.stock,
+                    price: props.price,
+                      qty:qty,
+                   img: props.thumbnail  }])
+            }}
 const clear = ()=> {
     setCartList([])
 }
